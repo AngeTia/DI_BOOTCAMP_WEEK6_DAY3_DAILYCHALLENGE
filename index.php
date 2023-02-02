f605c92fc4680ecfcee0cd333654731d1e302370
Gompou Tia Gbongué Ange <35264040+AngeTia@users.noreply.github.com>
2023-02-02 10:31:22 +0000

    Update

--- index.php
+++ index.php
@@ -10,32 +10,30 @@
         $units = $_POST['units'];
         if (!empty($units)) {
             $result = calculate_bill($units);
-            $result_str = 'Total amount of ' . $units . ' - ' . $result;
+            $result_str = 'Total amount of '.$units.' - '.$result;
         }
     }
     /**
      * To calculate electricity bill as per unit cost
      */
-    function calculate_bill($units) {
+    function calculate_bill($units)
+    {
         $unit_cost_first = 3.50;
         $unit_cost_second = 4.00;
         $unit_cost_third = 5.20;
         $unit_cost_fourth = 6.50;
 
-        if($units <= 50) {
+        if ($units <= 50) {
             $bill = $units * $unit_cost_first;
-        }
-        else if($units > 50 && $units <= 100) {
+        } else if ($units > 50 && $units <= 100) {
             $temp = 50 * $unit_cost_first;
             $remaining_units = $units - 50;
             $bill = $temp + ($remaining_units * $unit_cost_second);
-        }
-        else if($units > 100 && $units <= 200) {
+        } else if ($units > 100 && $units <= 200) {
             $temp = (50 * 3.5) + (100 * $unit_cost_second);
             $remaining_units = $units - 150;
             $bill = $temp + ($remaining_units * $unit_cost_third);
-        }
-        else {
+        } else {
             $temp = (50 * 3.5) + (100 * $unit_cost_second) + (100 * $unit_cost_third);
             $remaining_units = $units - 250;
             $bill = $temp + ($remaining_units * $unit_cost_fourth);
